a42dc66796d9529422c3834e149d52deef89e1c5
Vittorini <bzt.ivanov@gmail.com>
2024-11-17 18:38:18 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,16 +26,45 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = -2;
+        double c = 2;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+
+        if (a == 0 && b != 0 && c != 0) {
+            double x = -c / b;
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        double D = b * b - 4 * a * c;
+
+        if (D < 0 || (a == 0 && b == 0 && c != 0)) {
+            return "Количество решений: 0.";
+        }
+
+        if (D > 0) {
+            double x1 = (-b + Math.sqrt(D)) / 2 / a;
+            double x2 = (-b - Math.sqrt(D)) / 2 / a;
+
+            if(x1<x2) {
+                return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+            }
+
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
+        }
+
+        if (D == 0) {
+            double x = -b / 2 / a;
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        return null;
     }
 }
