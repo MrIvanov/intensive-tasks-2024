81a0a9d76e70d38a47d58a2d69e7981c2d5daecb
Vittorini <bzt.ivanov@gmail.com>
2024-11-17 19:49:58 +0300

    убрал пустую строку в isLeap(), убрал внешние скобки

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -36,7 +36,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-
-        return ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year > 0);
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year > 0;
     }
 }
