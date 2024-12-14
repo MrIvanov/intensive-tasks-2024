1ebc748cf34748d7755ce325e10072f4a02b9728
Vittorini <bzt.ivanov@gmail.com>
2024-12-15 00:40:25 +0300

    Решение с помощью Character class

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -11,11 +11,38 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("   "));
+        System.out.println(isPalindrome("  !!!!!! "));
+        System.out.println(isPalindrome(null));
+        System.out.println(isPalindrome("абвг 11 Гвба!"));
+        System.out.println(isPalindrome("Abc 11 cba!"));
+        System.out.println(isPalindrome("А роза. упала, на лапу Азора"));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        if (inputString == null || inputString.isBlank()) {
+            return false;
+        }
+
+        StringBuilder lowerCaseLiterals = new StringBuilder();
+        for (int i = 0; i < inputString.length(); i++) {
+            if (Character.isLetterOrDigit(inputString.charAt(i))) {
+                lowerCaseLiterals.append(Character.toLowerCase(inputString.charAt(i)));
+            }
+        }
+
+        int length = lowerCaseLiterals.length();
+        if (length < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < length / 2; i++) {
+            if (lowerCaseLiterals.charAt(i) != lowerCaseLiterals.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
